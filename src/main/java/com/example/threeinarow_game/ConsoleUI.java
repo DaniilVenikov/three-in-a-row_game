256d3538b74267f4b45f2ae3ce0414ba7e8d2dcd
DaniilVenikov <daniilvenikoff@gmail.com>
2025-07-30 15:40:15 +0300

    Added ConsoleUI ATD

---
+++ src/main/java/com/example/threeinarow_game/ConsoleUI.java
@@ -0,0 +1,8 @@
+package com.example.threeinarow_game;
+
+public abstract class ConsoleUI {
+    abstract void run();
+    abstract void printBoard();
+    abstract void printStats();
+    abstract void printError(String message);
+}
